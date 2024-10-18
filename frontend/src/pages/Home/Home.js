1792d5a0120e9b20749b690f5198204a22ca89b5
arunimasingh06 <154630845+arunimasingh06@users.noreply.github.com>
2024-10-19 03:59:14 +0530

    Update Home.js
    final without nav

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,34 +1,24 @@
 import React from "react";
-import "./Home.css";
-import image from "../../assets/images/hero-land-2.jpg";
+import "./home.css";
 
 export default function Home() {
-	return (
-		<section className="hero">
-			<div className="text-content">
-				<h1>Easy Corporate Commutes</h1>
-				<p>
-					Streamline ride-sharing, save on travel costs, and
-					contribute to a greener planet.
-				</p>
-				<div className="buttons">
-					{/* You can replace '#' with actual URLs */}
-					<a href="#" className="btn primary">
-						🚀 Host a ride
-					</a>
-					<a href="#" className="btn secondary">
-						Join a ride
-					</a>
-				</div>
-			</div>
-			<div className="image-content">
-				<img
-					src={image}
-					alt="Phone with UI"
-					className="phone-image"
-					style={{ borderRadius: "15px" }}
-				/>
-			</div>
-		</section>
-	);
+  return (
+    <section className="hero">
+      <div className="text-content">
+        <h1>Easy Corporate Commutes</h1>
+        <p>
+          Streamline ride-sharing, save on travel costs,
+		and contribute to a greener planet.
+        </p>
+        <div className="buttons">
+          <button className="btn primary">
+            🚀 Host a ride
+          </button>
+          <button className="btn secondary">
+            Join a ride
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
