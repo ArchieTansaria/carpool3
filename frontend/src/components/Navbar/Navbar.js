cde55ce16a547ad0da5bf2fd73a4fccf31f622d6
ArchieTansaria <archietans.work@gmail.com>
2024-10-19 03:28:17 +0530

    login functionality

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+	const navigate = useNavigate(); // Create a navigation function
+
 	return (
 		<div className="navbar">
 			<div className="logo">
@@ -13,7 +16,9 @@ export default function Navbar() {
 				<a href="#contact" className="contact">
 					Contact
 				</a>
-				<button className="log-in">Log in</button>
+				<button className="log-in" onClick={() => navigate("/login")}>
+					Log in
+				</button>
 				<button className="sign-up">Sign up</button>
 			</div>
 		</div>
